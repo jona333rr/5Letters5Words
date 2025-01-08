82ac0f35803c7bb9188dc4f06ec0a626e24192a0
Jonathan Noah Pedersen <jona333r@edu.sde.dk>
2025-01-08 10:20:52 +0100

    Dictionary + Letts & Words to bit

--- 5Letters5Words/Program.cs
+++ 5Letters5Words/Program.cs
@@ -5,12 +5,12 @@ using System.Text;
 
 //Jeg skal til at i gang med at lave bitMap. 
 
-string[] _imPerfectWords = File.ReadAllLines("./new_beta.txt");
+string[] _imPerfectWords = File.ReadAllLines("./words_alpha.txt");
 
 
 
 
-List<string> _perfectWords = new List<string>();
+Dictionary<int, int> _perfectWords = new();
 
 
 
@@ -34,62 +34,50 @@ for (int i = 0; i < _imPerfectWords.Length; i++)
 
         }
         if (chars.Count != 5) continue;
-
-        if (UsedChars(chars, _perfectWords))
-            continue;
-        try
-        {
-            _perfectWords.Add(word);
-            
-        }
-        catch (Exception ex) 
-        {
-            Console.WriteLine((ex) + " " +  "Adding word to the list failed");
-        }
-   
-
+        var bittedWord = wordToBit(word);
+        if (_perfectWords.ContainsKey(bittedWord)) continue;
+        _perfectWords.Add(bittedWord, bittedWord);
     }
 }
 
 
 const int wordCount = 5;
 int solutions = 0;
-matchingWords(_perfectWords.ToArray(), wordCount, _perfectWords.Count, new List<string>());
+matchingWords(_perfectWords.Values.ToArray(), wordCount, _perfectWords.Count, 0, 0);
 
-void matchingWords(string[] words, int wordCount, int index, List<string> selectedWords)
+void matchingWords(int[] words, int wordCount, int index, int usedChars, int currentDepth)
 {
-    if (selectedWords.Count == wordCount)
+    if (currentDepth == wordCount)
     {
         solutions++;
-        Console.WriteLine(string.Join(" ", selectedWords));
         Console.WriteLine(solutions);
     }
     for (int i = index - 1; i >= 0; i--)
     {
-        if ( string.Concat(string.Join("", selectedWords), words[i]).Distinct().Count() != 5 * (selectedWords.Count + 1)) continue;
-        List<string> nextSelectedWord = new List<string>(selectedWords);
-        nextSelectedWord.Add(words[i]);
+        if ((usedChars & words[i]) > 0) continue;
         
-        matchingWords(words, wordCount, i, nextSelectedWord);
+        
+        matchingWords(words, wordCount, i, usedChars | words[i], currentDepth + 1);
         
     }
 }
 
 
 
-bool UsedChars(List<char> chars, List<string> perfectWords)
+int charToBit (char character)
 {
-    for (int i = 0; i < perfectWords.Count; i++)
-    {
-        string word = perfectWords[i];
+    return 1 << (character - 'a');
+}
 
-        for (int j = 0; j < word.Length; j++)
-        {
-            if (!chars.Contains(word[j])) break;
-            if (j == 4) return true;
-        }
+
+int wordToBit (string Words)
+{
+    int WordRepresentation = 0;
+
+    for (int i = 0; i < Words.Length; i++) 
+    {
+        WordRepresentation |= charToBit(Words[i]);    
     }
 
-
-    return false;
+    return WordRepresentation;
 }
